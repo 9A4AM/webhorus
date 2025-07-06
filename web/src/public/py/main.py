c4dd4481b8c172b84772d735f8b8fdfc3548d972
xssfox <xss@sprocketfox.io>
2025-07-06 13:43:38 +1000

    bias-t fix? maybe

--- web/src/public/py/main.py
+++ web/src/public/py/main.py
@@ -11,7 +11,7 @@ import logging
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
-VERSION = "0.0.15"
+VERSION = "0.0.16"
 
 # horus_demod = demod.Demod(tone_spacing=int(document.getElementById("tone_spacing").value))
 
